c38b9504587b54972760115c95d59f13791cbc24
sohatach <sohatach@gmail.com>
2018-12-16 23:36:43 +0900

    fix spider

--- money_crawl/money_crawl/spiders/ESolSpider.py
+++ money_crawl/money_crawl/spiders/ESolSpider.py
@@ -4,8 +4,17 @@ import scrapy
 
 class EsolspiderSpider(scrapy.Spider):
     name = 'ESolSpider'
-    allowed_domains = ['https://ssl4.eir-parts.net/V4Public/EIR/4420/ja/announcement/announcement_1.xml']
-    start_urls = ['http://https://ssl4.eir-parts.net/V4Public/EIR/4420/ja/announcement/announcement_1.xml/']
+    allowed_domains = ['https://ssl4.eir-parts.net']
+    start_urls = ['https://ssl4.eir-parts.net/V4Public/EIR/4420/ja/announcement/announcement_1.xml/']
 
     def parse(self, response):
-        pass
+
+        item = MoneyCrawlItem()
+        ##response.selector.remove_namespaces() # これをやらないとデータが見えない
+        
+        item['title'] =response.xpath('//item/title/text()').extract()
+        item['link'] =response.xpath('//item/link/text()').extract()
+        item['pub_date'] =response.xpath('//item/pubDate/text()').extract()
+        
+        yield item
+
